2a9549beaf3865a5e2add3cedbc5156e5f42d247
12th-ai <mihigofololive@gmail.com>
2024-11-25 18:05:28 +0200

    update: set proxy server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000', // Your backend server URL
+                changeOrigin: true,
+                secure: false, // If using HTTPS, set to true
+            },
+        },
+    },
 })
